153dda87adc7877afaaa74e257a7046057c1644e
Justin Dacanay <jdacanay@permission.io>
2022-12-02 14:06:30 +0800

    added new test scenarios

--- backend/backend/tests/test_views.py
+++ backend/backend/tests/test_views.py
@@ -6,6 +6,22 @@ class TestViews(TestSetup):
         response = self.client.post(self.fibonacci_url)
         self.assertEqual(response.status_code, 400)
 
+    def test_not_positive_integer_1(self):
+        response = self.client.post(self.fibonacci_url, {"n": -1}, format="json")
+        self.assertEqual(response.data, {'status': 'Invalid', 'message': 'n must be a positive integer'})
+        self.assertEqual(response.status_code, 400)
+    
+    def test_not_positive_integer_2(self):
+        response = self.client.post(self.fibonacci_url, {"n": "a"}, format="json")
+        self.assertEqual(response.data, {'status': 'Invalid', 'message': 'n must be a positive integer'})
+        self.assertEqual(response.status_code, 400)
+
     def test_happy_path_fibonacci_status_201(self):
         response = self.client.post(self.fibonacci_url, self.mock_data['request_body'], format="json")
+        self.assertEqual(response.data, {'nth': '5', 'status': 'Success'})
         self.assertEqual(response.status_code, 201)
+
+    # def test_happy_path_fibonacci_status_201_long(self):
+    #     response = self.client.post(self.fibonacci_url, {"n": 10000}, format="json")
+    #     self.assertEqual(response.data, {})
+    #     self.assertEqual(response.status_code, 201)
